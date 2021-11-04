b9521514d52732c8bb28b732c12c2994f4ec6901
bearni95 <bernatcanal@gmail.com>
2021-11-04 11:25:16 +0100

    cleanup

--- dist/types/HttpGateway.d.ts
+++
@@ -1,7 +0,0 @@
-import { StreamrGet } from './StreamrGet';
-export declare class HttpGateway {
-    port: number;
-    getter: StreamrGet;
-    constructor(privateKey: string, streamId: string, port: number);
-    listen(): void;
-}
--- dist/types/index.d.ts
+++
@@ -1,4 +0,0 @@
-export * from './storage/Storage';
-export * from './storage/MemoryStorage';
-export * from './HttpGateway';
-export * from './StreamrGet';
